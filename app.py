1ebc44dbd91a2f4b105edb1a57d6cf785979eee6
summerxiahuang <xia.summer.huang@gmail.com>
2023-08-29 11:38:19 -0400

    revised app

--- app.py
+++ app.py
@@ -1,90 +1,116 @@
-# index page
 from dash import dcc, html, Input, Output, State
-import dash_bootstrap_components as dbc
 from my_server import app
 from flask_login import logout_user, current_user
-from views import login, login_fd, logout, account, session, joblist_unity
+from flask import session
+import dash_bootstrap_components as dbc
+from views import login, account, joblist_unity, project, new_job
 
-header = html.Div(
-    className='header',
-    children=html.Div(
-        # className='container-width',
-        style={'height': '100%'},
-        children=[
-            html.H4('JOB RUNNER'),
-            html.Div(className='links', children=[
-                html.Div(id='current-joblist', className='link'),
-                html.Div(id='user-name', className='link'),
-                html.Div(id='logout', className='link')
-            ]),
-        ],
-
-    ), style={'margin': 20}
+link_bar = dbc.Row(
+    [
+        dbc.Col(id='current-joblist', width='auto'),
+        dbc.Col(id='user-name', width='auto'),
+        dbc.Col(id='logout', width='auto'),
+        dbc.Col(
+            dbc.Row(
+                [
+                    dbc.Col(html.I(className="bi bi-question-circle-fill"), width="auto"),
+                    dbc.Col(dbc.NavLink("Help", href="/help"), width="auto"),
+                ],
+            ),
+        )
+    ],
+    className='ms-auto flex-nowrap mt-3 mt-md-3 me-5', align="center",
 )
 
+navbar = dbc.Navbar(
+    [
+        html.A(
+            dbc.Row(
+                [dbc.Col(html.Img(src='/assets/lmt_img.jpg', height='30px'), ),
+                 dbc.Col(
+                     dbc.NavbarBrand('JOB RUNNER', className='ms-2', style={'fontSize': '24px', 'color': 'black'})), ],
+                # ms meaning margin start
+                align='right',
+                className='ms-5'
+            ),
+            href='/account', style={'textDecoration': 'none'}
+        ),
+        dbc.NavbarToggler(id='navbar-toggler', n_clicks=0),
+        dbc.Collapse(
+            link_bar,
+            id='navbar-collapse',
+            is_open=False,
+            navbar=True
+        )
+    ],
+    dark=True
+)
 app.layout = html.Div(
     [
-        header,
+        navbar,
         html.Br(),
-        html.Div([
-            html.Div(
-                html.Div(id='control-content', className='content'),
-                className='content-container'
-            ),
-        ],
-            # className='container-width'
-        ),
-
+        html.Div(id='body-content', className='content'),
+        # keep track of the current URL, the app will handle the location change without a full page refresh
         dcc.Location(id='url', refresh=False),
     ]
 )
 
 
-@app.callback(Output('control-content', 'children'),
+# update the body-content children based on the URL
+@app.callback(Output('body-content', 'children'),
               [Input('url', 'pathname')])
 def display_page(pathname):
-    if pathname == '/':
+    auth_routes = {
+        '/account': account.layout,
+        '/project': project.layout,
+        '/newjob': new_job.layout,
+        # '/newjobtab': new_job_tab.layout,
+    }
+    if pathname in ['/', '/login']:
         return login.layout
-    elif pathname == '/login':
-        return login.layout
-    elif pathname == '/account':
-        if current_user.is_authenticated:
-            return account.layout
-        else:
-            return login_fd.layout
-    elif pathname == '/session':
-        if current_user.is_authenticated:
-            return session.layout
-        else:
-            return login_fd.layout
+    elif pathname in auth_routes and current_user.is_authenticated:
+        return auth_routes[pathname]
     elif pathname == '/joblist_unity':
         return joblist_unity.layout
     elif pathname == '/logout':
         if current_user.is_authenticated:
             logout_user()
-            return logout.layout
-        else:
-            return logout.layout
+            session.clear()
+        return login.layout
+    elif not current_user.is_authenticated:
+        return dcc.Location(pathname='/login', id='url_redirect')
     else:
         return '404'
 
 
+# update the navbar
 @app.callback(
-    Output('user-name', 'children'),
-    Output('logout', 'children'),
-    Output('current-joblist','children'),
-    [Input('control-content', 'children')])
-def cur_user(input1):
+    [Output('user-name', 'children'),
+     Output('logout', 'children'),
+     Output('current-joblist', 'children')],
+    [Input('body-content', 'children')])
+def nav_bar(input1):
     if current_user.is_authenticated:
-        return html.A('Current user: ' + current_user.username, href='/account'), \
-               html.A('Logout', href='/logout'),\
-               html.A('Current running jobs', href='/joblist_unity')
-        # 'User authenticated' return username in get_id()
+
+        return (dbc.NavLink('Current user: ' + current_user.username, href='/account'),
+                dbc.NavLink('Logout', href='/logout', ),
+                dbc.NavLink('Current running jobs', href='/joblist_unity'))
     else:
         return '', '', ''
 
 
+# add callback for toggling the collapse on small screens
+@app.callback(
+    Output("navbar-collapse", "is_open"),
+    [Input("navbar-toggler", "n_clicks")],
+    [State("navbar-collapse", "is_open")],
+)
+def toggle_navbar_collapse(n, is_open):
+    if n:
+        return not is_open
+    return is_open
+
+
 # export FLASK_ENV=development
 if __name__ == '__main__':
-    # app.run_server()
     app.server.run(port='8000', debug=True)
