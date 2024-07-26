d18538c6ab6ff7632dc0f001ba4f526a48314981
Xia Huang <xia.summer.huang@gmail.com>
2024-07-26 10:49:54 -0400

    change dir

--- functions/project_function.py
+++ functions/project_function.py
@@ -486,10 +486,12 @@ def make_progress_graph(progress, total):
 
 def submit_job(runfile, default_work_lmt,pid):
 
-    pid_path = os.path.join(default_work_lmt, 'lmtoy_run', f'lmtoy_{pid}')
-
+    #pid_path = os.path.join(default_work_lmt, 'lmtoy_run', f'lmtoy_{pid}')
+    runfile_path = os.path.dirname(runfile)
+    print('runfile:', runfile)
+    print('pid_path:', runfile_path)
     result = subprocess.run(['sbatch_lmtoy.sh ', runfile], capture_output=True,
-                            text=True, cwd=pid_path)
+                            text=True, cwd=runfile_path)
     return result
 
 # def edit_row_details(details_data):
